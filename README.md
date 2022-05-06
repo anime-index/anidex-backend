99fa12cf36e7a6dae4e023f3265d8546b0202262
Andreu Vall Hernàndez <andreu.vallhernandez@gmail.com>
2022-05-06 20:11:36 +0200

    It's working man

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # anidex-backend
 
 Backend REST api for the website, using Python's Fast API
+
+Data from: https://www.kaggle.com/datasets/abhishtagatya/my-anime-list-2021
+
+
+Serach later: MySQL cloud hosting
--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 from typing import Optional
-
 from fastapi import FastAPI
+
+import pandas as pd
+
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000"
+]
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+animes = pd.read_json('data/animes.json')
+series = pd.read_json('data/series.json')
+
 
 @app.get('/')
-def read_root():
+def read_root(q: Optional[str] = None):
     return {'Hello': 'World'}
 
+@app.get('/top/anime')
+def get_top_anime():
+    return animes.sort_values('mal_score', ascending=False).fillna('').head(20).to_dict(orient='records')
 
-@app.get('/items/{item_id}')
-def read_item(item_id: int, q: Optional[str] = None):
-    return {'item_id': item_id, 'q': q}
+@app.get('/top/series')
+def get_top_series():
+    return series.sort_values('score', ascending=False).head(20).to_dict(orient='records')
+
+@app.get('/series/{item_id}')
+def get_franchise(item_id: int):
+    return series.loc[series.series_id==item_id].to_dict(orient='index')[item_id]
